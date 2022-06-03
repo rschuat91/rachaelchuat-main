d0438f85d17714ca3ca0690f56d71424ebca7aa6
rschuat91 <91403119+rschuat91@users.noreply.github.com>
2022-06-03 20:44:34 +0800

    Add files via upload

---
+++ rachaelchuat-main/footer.php
@@ -0,0 +1,14 @@
+
+
+
+<section class="footer">
+  <img src="images/fremantleartslogo.png" alt="Logo" style="width:150px;">
+  <p> © 2022 Fremantle Arts Centre. All rights reserved.</p>
+  <img src="images/socialmedia.png" alt="social media" style="width:100px;">
+  </section>
+
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ rachaelchuat-main/index.php
@@ -0,0 +1,67 @@
+
+?php get_header();?>   <!--  Tells WordPress to include header.php -->
+<!-- Whats on events -->
+
+<section class="whatsonevents">
+
+<div class="row">
+  <div class="col content col">
+
+<h2>WHAT'S ON EVENT?</h2>
+<h3>SUNDAY MUSIC: Sunday Music is FAC’s free weekly live music program. 
+  Enjoy the best local and touring bands every weekend in the beautiful setting of FAC’s Front Garden. 
+  Bring a rug and picnic to this family friendly event, or enjoy wood fired pizzas and drinks from our bar. 
+  Some chairs provided.</h3>
+
+  </div>
+<div class="col">
+<div class="imagegallery">
+<img src="http://206.189.45.97/~mesh5/" alt="whatsonevent" style="width:300px">
+
+</div>
+</div>
+</div>
+</section>
+
+<section>
+
+</div>
+<div class="col">
+<div class="imagegallery">
+<img src="http://206.189.45.97/~mesh5/" alt="sancisco" style="width:300px">
+</div>
+</div>
+
+
+<div class="row-cols-lg-1">
+  <div class="Artist">
+<h2>SAN CISCO</h2>
+<h3>SATURDAY 11 JUNE 2022|7.00 PM</h3>
+<h4>Join SAN CISCO on their latest single and tour.
+   After 2 years of covid and endless cancellations,
+    San cisco decided to come back and join us.</h4>
+
+  </div>
+  </div>
+
+</div>
+
+<div class="col">
+<div class="imagegallery">
+<img src="http://206.189.45.97/~mesh5/" alt="kingstingray2" style="width:300px">
+</div>
+</div>
+</div>
+
+<div class="row-cols-lg-1">
+  <div class="Artist">
+<h2>KING STINGRAY</h2>
+<h3>THURSDAY - 30TH JUNE 2022|7.00PM - 10.00PM</h3>
+<h4>The 2022 King Stingray tour is set to kick off in Fremantle at FAC, 
+  before making its way through Perth in June.</h4>
+
+  </div>
+  </div>
+
+</section>
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
