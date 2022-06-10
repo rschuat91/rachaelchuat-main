39c5a594984be1c018a0fdee0710a9a56fa95559
rschuat91 <91403119+rschuat91@users.noreply.github.com>
2022-06-10 17:52:09 +0800

    Add files via upload

---
+++ rachaelchuat-main/rachaelchuat-main/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
+
+<title><?php
+  if (function_exists('is_tag') && is_tag()) {
+     single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+  elseif (is_archive()) {
+     wp_title(''); echo ' Archive - '; }
+  elseif (is_search()) {
+     echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+  elseif (!(is_404()) && (is_single()) || (is_page())) {
+     wp_title(''); echo ' - '; }
+  elseif (is_404()) {
+     echo 'Not Found - '; }
+  if (is_home()) {
+     bloginfo('name'); echo ' - '; bloginfo('description'); }
+  else {
+      bloginfo('name'); }
+  if ($paged>1) {
+     echo ' - page '. $paged; }
+?>
+
+</title>
+
+<meta name="description" content="<?php bloginfo('description'); ?>">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
+
+
+</head>
+
+<body>
+
+ <!-- Toggler/collapsibe Button -->
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+
+<!-- navbar -->
+
+
+<nav class="navbar">
+  <img src="images/fremantleartslogo.png" alt="Logo" style="width:150px;">
+   <ul class="nav-links">
+    <li  class="active"><a href="#"></a>Home</a></li>
+    <li><a href="#"></a>Events</a></li>
+    <li><a href="#"></a>Booking</a></li>
+
+    <form class="form-inline"action="/action_page.php">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+
+    </ul>
+
+<header>
+ <div class="tag">
+  <h1>FREMANTLE ARTS </h1>
+    <h1>CONCERT</h1>
+
+
+ </div>
+
+ <?php wp_head(); ?> <!-- very important that this is added--> 
+
+</header>
